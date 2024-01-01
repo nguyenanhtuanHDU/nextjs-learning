90b9b16d73c7a99a35b50e752644f933e0e2ed55
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2024-01-01 20:39:59 +0700

    useRouter base

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 'use client'
 
-import { INavigatorProps } from "./shared/models/Navigator";
 import Navigator from "./components/navigator";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
 
+  const handleGoUserRoute = () => {
+    if (confirm("Go to User route?")) {
+      router.push('/user') // điều hướng đến route /user, thêm route /user vào history
+      // router.replace('/user') // điều hướng đến route /user, replace route hiện tại
+    }
+  }
 
   return (
     <>
-      <Navigator></Navigator>
+      <Navigator />
       <div>Home</div>
+      <button onClick={() => handleGoUserRoute()}>go user</button>
     </>
   )
 }
